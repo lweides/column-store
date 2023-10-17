80f3c303529e25558a45570f83fb31fb0c9b875b
Lukas Weidinger <lukas.weides@gmail.com>
2023-10-17 11:25:00 +0200

    #6: initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("me.champeau.jmh") version "0.7.1"
+}
+
+group = "column.store"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
